83d2651fedaeeeb1961b3c4f8bfbaa704e4b8159
Nitikagupta16 <58361725+Nitikagupta16@users.noreply.github.com>
2020-07-19 00:45:01 +0530

    Update script.js

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -101,14 +101,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory (categories);
-      $ajaxUtils.sendGetRequest(
-        menuItemHtml,
-        function (menuItemHtml) {
-      // Switch CSS class active to menu button
-          switchMenuToActive();
-
-          
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+     
+      
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -121,7 +116,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //var homeHtmlToInsertIntoMainPage = ...
-         var homeHtmlToInsertIntoMainPage = 
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+         var homeHtmlToInsertIntoMainPage = insertHtml(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
              
           
 
